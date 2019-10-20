36f1ffe1912a8c1958c2afd0481ffc2b9de346fa
Riley Moore <rilemoore@gmail.com>
2019-10-20 17:29:58 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    sorted_classword = @word.sort
+    array_successes = []
+    array.each do |word|
+      if word.sort
+    end
+  end
+  
+end
